31c736b5fb5fc3f6d7409ec170d534e2fe952c34
admin <admin@adm.in>
2020-04-17 00:06:48 +0300

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import url
-
+from django.urls import path
 from . import views
 
-app_name = 'polls'
+app_name = 'posts'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    path('', views.post_list, name='post_list'),
 ]
