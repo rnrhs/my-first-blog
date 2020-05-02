505195180d4e131c4542af3e80853638420f5c4d
admin <admin@adm.in>
2020-05-02 17:35:04 +0300

    Add remove and draft buttons, authorization.

--- Site/settings.py
+++ Site/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+LOGIN_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'likes',
     'blog',
 ]
 
